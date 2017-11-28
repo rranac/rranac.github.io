5639233ff8d95a906e48406d99efb3dd4c845568
rranac <ritarana7594@gmail.com>
2017-11-28 13:34:54 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+	//navigation
+$(function() {
+		$(document).ready(function(){
+  		$('.nav li a').click(function() {
+    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    	&& location.hostname == this.hostname) {
+      	var $target = $(this.hash);
+      	$target = $target.length && $target
+      	|| $('[name=' + this.hash.slice(1) +']');
+      	if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      		  }
+			}
+  		});
+	});
+	
+//images link  
+$("button").click(function(){		
+	$('html,body').animate({
+		scrollTop: $($(this).attr('href')).offset().top}, 1700);
+	});
+
+		
+
+})(jQuery);
